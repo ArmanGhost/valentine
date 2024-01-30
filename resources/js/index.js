c52ea9b5c70607e76f388332fa0473f0848e6ca2
Arman Ryabinin <aryabinin@ncl.com>
2024-01-30 16:39:26 +0600

    Update

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,28 @@
 // alert("Only for RENATOCHKA!");
 
-
+$(document).ready(function() {
+    $("#yes-button").click(function(){
+        $("#flower").hide()
+        $("#pistol").hide()
+        $("#dancing").show()
+        $("#hey").hide()
+        $("#will").hide()
+        $("#no").hide()
+        $("#see").show()
+        $("#purpur").show()
+        $("#yes-button").hide()
+        $("#no-button").hide()
+    });
+    $("#no-button").click(function(){
+        $(this).addClass("no-button");
+        $("#flower").hide()
+        $("#pistol").show()
+        $("#hey").hide()
+        $("#will").hide()
+        $("#no").show()
+        $(this).animate({
+            'top': Math.random() * window.innerHeight + "px",
+            'left': Math.random() * window.innerWidth + "px"
+          }, "medium");
+    }); 
+});
